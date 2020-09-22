86f6b7ad969cc47d960acaa0eeb064af5e5b41d2
jose.rubio5317@alumnos.udg.mx <jose.rubio5317@alumnos.udg.mx>
2020-09-22 13:06:18 -0500

    Revert "Comentamos el Hola Mundo" Se necesita que imprima el Hola Mundo
    This reverts commit c3e40cd0ddaed940ff34e740083de551fc954878.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@ int main(){
     char nombre[100];
     unsigned int edad;
 
-    //printf("Hola Mundo!\n");
+    printf("Hola Mundo!\n");
     printf("Nombre: ");
     fgets(nombre,sizeof(nombre),stdin);
     printf("Hola %s",nombre);
